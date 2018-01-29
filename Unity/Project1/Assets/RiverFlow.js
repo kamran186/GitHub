bf421aac4cab2967c8ad6174db8e502b74eca49b
kamran186 <33045559+kamran186@users.noreply.github.com>
2018-01-29 12:51:59 +0500

    Learning Unity

---
+++ Unity/Project1/Assets/RiverFlow.js
@@ -0,0 +1,9 @@
+﻿var scrollSpeed_x=0.3;
+var scrollSpeed_y=0.3;
+function Update() {
+    var offsetX= Time.time*scrollSpeed_x;
+    var offsetY= Time.time*scrollSpeed_y;
+
+    renderer.material.mainTextureOffset=Vector2(offsetY,offsetX);
+
+}
